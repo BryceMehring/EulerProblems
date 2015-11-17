1f673b579a2022be49fe3220a8cfd6c0501543dc
Bryce Mehring <brycemehring@gmail.com>
2015-11-17 14:21:14 -0600

    Added 4th solution

---
+++ source/04/main.py
@@ -0,0 +1,11 @@
+def main():
+    largestNumber = 0
+    for a in range(100, 1000):
+        for b in range(100, 1000):
+            product = a * b
+            productStr = str(a * b)
+            if productStr == productStr[::-1]:
+                largestNumber = max(largestNumber, product)
+    print largestNumber
+
+main()
